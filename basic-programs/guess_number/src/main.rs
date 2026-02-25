794d649be8e7801220a1fed3237264b5839b9edf
jzargo <z.ishanov04@gmail.com>
2026-02-25 14:44:34 +0500

    Initial commit with Rust projects: helloworld and number guesser

---
+++ basic-programs/guess_number/src/main.rs
@@ -0,0 +1,36 @@
+use rand::Rng; 
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number!");
+    
+    let secret = rand::thread_rng().random_range(1..=100); 
+
+    
+
+    loop {
+        println!("Enter your guess: ");
+        
+        let mut guess = String::new();
+
+        std::io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read a line!");
+
+        let guess: u32 = match guess
+            .trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+            
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+    
+}
